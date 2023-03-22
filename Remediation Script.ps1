a8c1e13d98d507b6814262342b32911e31338156
Industrya <128462845+Industrya@users.noreply.github.com>
2023-03-22 11:03:54 +0100

    Update Remediation Script.ps1

--- Remediation Script.ps1
+++ Remediation Script.ps1
@@ -3,7 +3,7 @@ $TempDir = [System.IO.Path]::GetTempPath()
 New-Item -ItemType Directory -Force -Path "$TempDir\HP"
 cd "$TempDir\HP"
 
-if (-not (Test-Path -path "C:\Program Files\HP\HP Firmware Installer\HP USB-C Dock G")) {
+if (-not (Test-Path -path "C:\Program Files\HP\HP Firmware Installer\HP USB-C Dock G5")) {
 Get-Softpaq -number 143343 -Extract -DestinationPath "c:\SWsetup\SP143343" -verbose
 msiexec /i "C:\SWSetup\SP143343\Manageability\HPFirmwareInstaller64.msi" /qn /L*v C:\swsetup\dock.log
 } else{
